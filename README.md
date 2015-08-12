57dc6e66e1ec6062fc542f328ace0d3c4faa3cc4
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2015-08-12 09:28:37 -0400

    Commit test

--- README.md
+++ README.md
@@ -4,12 +4,4 @@ Here is the online version of weapons array that I am charged to work with.
 
 # TO-Do #
 
-Make ui
-
-
-# Included #
-- Passgen
-- Calc
-- Converter
-- TestConsole
-- Phone#Decoder
+Fix Stuff
---
+++ descript.js
@@ -0,0 +1,54 @@
+
+
+
+//onClick="location.href='http://www.google.com'"
+function calcDes() {             
+                
+                document.getElementById("calcButton").style.background='#4E194E';
+                document.getElementById("des").innerHTML = "Calculator: Calculator with basic math functions.";
+                
+            }
+function passDes() {
+                document.getElementById("passgenButton").style.background='#4E194E';
+    document.getElementById("des").innerHTML = "Password Generator: This application generates passwords with settings that you input.";
+                
+            }
+function convertDes() {
+                document.getElementById("converterButton").style.background='#4E194E';
+    document.getElementById("des").innerHTML = "Converter: Convert Hex,Bin";
+                
+            }
+/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
+/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
+/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
+            function removeCalcText() {
+                document.getElementById("calcButton").style.background='purple';
+            document.getElementById("des").innerHTML = " ";   
+            
+            }
+function removePassText() {
+                document.getElementById("passgenButton").style.background='purple';
+            document.getElementById("des").innerHTML = " ";   
+            
+            }
+function removeConvText() {
+    
+    
+                document.getElementById("converterButton").style.background='purple';
+            document.getElementById("des").innerHTML = " ";   
+            
+            }
+
+
+
+
+
+
+
+/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-*/
+/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-*/
+function animDes() {
+ //   alert("started!");
+ //    document.getElementById("descript").style.height = '200px';
+}
+animDes();
