0b72d1a27ad1062611b77a4272ca29749d373718
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2015-08-15 11:54:04 -0400

    changing the toolbox UI for the new one, that I take credit, but inspiration comes from HTMLGUY, who offered me an idea but I took the divs from it and changed the whole UI for me.

--- descript.js
+++ descript.js
@@ -5,37 +5,32 @@
 function calcDes() {             
                 
                 document.getElementById("calcButton").style.background='#4E194E';
-                document.getElementById("des").innerHTML = "Calculator: Calculator with basic math functions.";
+                
                 
             }
 function passDes() {
                 document.getElementById("passgenButton").style.background='#4E194E';
-    document.getElementById("des").innerHTML = "Password Generator: This application generates passwords with settings that you input.";
-                
+                 
             }
 function convertDes() {
                 document.getElementById("converterButton").style.background='#4E194E';
-    document.getElementById("des").innerHTML = "Converter: Convert Hex,Bin";
-                
+              
             }
 /* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
 /* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
 /* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
             function removeCalcText() {
                 document.getElementById("calcButton").style.background='purple';
-            document.getElementById("des").innerHTML = " ";   
-            
+             
             }
 function removePassText() {
                 document.getElementById("passgenButton").style.background='purple';
-            document.getElementById("des").innerHTML = " ";   
             
             }
 function removeConvText() {
     
     
                 document.getElementById("converterButton").style.background='purple';
-            document.getElementById("des").innerHTML = " ";   
             
             }
 
@@ -47,8 +42,3 @@ function removeConvText() {
 
 /* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-*/
 /* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-*/
-function animDes() {
- //   alert("started!");
- //    document.getElementById("descript").style.height = '200px';
-}
-animDes();
