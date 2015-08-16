cfe66ba1a260851f1d0d46731f85826f32bb1c47
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2015-08-16 14:43:23 -0400

    added second div

--- descript.js
+++ descript.js
@@ -2,43 +2,72 @@
 
 
 //onClick="location.href='http://www.google.com'"
-function calcDes() {             
-                
-                document.getElementById("calcButton").style.background='#4E194E';
-                
-                
-            }
-function passDes() {
-                document.getElementById("passgenButton").style.background='#4E194E';
-                 
-            }
-function convertDes() {
-                document.getElementById("converterButton").style.background='#4E194E';
-              
-            }
-/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
-/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
-/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER- */
-            function removeCalcText() {
-                document.getElementById("calcButton").style.background='purple';
-             
-            }
-function removePassText() {
-                document.getElementById("passgenButton").style.background='purple';
-            
-            }
-function removeConvText() {
+
+/** Hover Text */
+function calcDes() {                             document.getElementById("calcButton").style.background='#4E194E';
+                   }
+function passDes() {                document.getElementById("passgenButton").style.background='#4E194E';
+                   }
+function convertDes() { document.getElementById("converterButton").style.background='#4E194E';
+                      }
+function removeCalcText() {        document.getElementById("calcButton").style.background='purple';
+                          }
+function removePassText() {          document.getElementById("passgenButton").style.background='purple';
+                          }
+function removeConvText() {    document.getElementById("converterButton").style.background='purple';
+                          }
+/** Hover Text */
+
+//Div 1 Code
+function divONECALC() {
+var calcDIVONE = document.createElement('iframe');
+calcDIVONE.src = "CalculatorWebView.html"
+document.querySelector('#main1').appendChild(calcDIVONE);
+}
+
+function divONEPass() {
+ 
+}
+
+function divONEConv() {
+
+}
+
+function divONEConsole() {
+
+}
+
+function divONEPhoneDec() {
     
+}
+
+function divONEstats() {
     
-                document.getElementById("converterButton").style.background='purple';
-            
-            }
+}
 
+//Div 2 Code
+function divTWOCALC() {
+var calcDIVTWO = document.createElement('iframe');
+calcDIVTWO.src = "CalculatorWebView.html"
+document.querySelector('#main2').appendChild(calcDIVTWO);
+}
 
+function divONEPass() {
+ 
+}
 
+function divONEConv() {
 
+}
 
+function divONEConsole() {
 
+}
 
-/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-*/
-/* BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-BLOCKER-*/
+function divONEPhoneDec() {
+    
+}
+
+function divONEstats() {
+    
+}
