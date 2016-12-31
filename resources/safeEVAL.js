3535ec00e4f3d9d19efbaa698ff657dd706cf884
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-12-31 15:01:10 -0500

    fixed spelling error

---
+++ resources/safeEVAL.js
@@ -0,0 +1,8 @@
+/**
+A CC0 library made by Damian Breitenberg.
+I grew tired of eval being misued on my programs, so I have hacked together a simple eval interpreter to block out malicious code injection.
+*/
+function seval(str) {
+    //setting regexp
+
+}
