9ca9a29c3f01e8b3add63d8959d9a29da3afc51a
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2017-01-01 13:08:36 -0500

    testing

--- resources/safeEVAL.js
+++ resources/safeEVAL.js
@@ -3,6 +3,14 @@ A CC0 library made by Damian Breitenberg.
 I grew tired of eval being misued on my programs, so I have hacked together a simple eval interpreter to block out malicious code injection.
 */
 function seval(str) {
-    //setting regexp
+
+    str = str.split(" ").join(""); //removing spaces
+    //remove numbers
+    // /([0-9+/*-])+/g
+
+    //Single out formula
+    //(Math.[A-z]+[(][0-9+/*-]+[)])
+
+
 
 }
